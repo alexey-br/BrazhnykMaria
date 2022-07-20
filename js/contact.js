54b8c8d145f09077690814dc4ea5fcf90bce0b54
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-20 22:42:37 +0300

    закрытие модалки Escape

--- js/contact.js
+++ js/contact.js
@@ -11,14 +11,24 @@ function onOpenContactModalClick(e) {
   }
 
   contactModalRef.addEventListener('click', onContactModalClose);
+  document.addEventListener('keydown', onContactModalEscClose);
 }
 
 function onContactModalClose(e) {
   if (e.target !== e.currentTarget) return;
 
+  closeContactModal();
+}
+
+function onContactModalEscClose(e) {
+  if (e.code === 'Escape') closeContactModal();
+}
+
+function closeContactModal() {
   if (!contactModalRef.classList.contains('backdrop--is-hidden')) {
     contactModalRef.classList.add('backdrop--is-hidden');
   }
 
   contactModalRef.removeEventListener('click', onContactModalClose);
+  document.removeEventListener('keydown', onContactModalEscClose);
 }
