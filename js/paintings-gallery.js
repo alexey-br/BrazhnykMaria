10b5bcb69d0ab1c09fb5edee7acf526de572eaef
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-07-26 21:09:59 +0300

    добавлены картины в портфолио

--- js/paintings-gallery.js
+++ js/paintings-gallery.js
@@ -1,71 +1,224 @@
 const paintingsGalleryData = [
-  { 
-    original: './images/portfolio/painting-1.jpg',
-    original2x: './images/portfolio/painting-1.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-2.jpg',
-    original2x: './images/portfolio/painting-2.jpg',
-    description: 'картина',
-  },
-  { 
+  // {
+  //   original: './images/portfolio/painting-1.jpg',
+  //   original2x: './images/portfolio/painting-1.jpg',
+  //   description: 'картина',
+  // },
+  // {
+  //   original: './images/portfolio/painting-2.jpg',
+  //   original2x: './images/portfolio/painting-2.jpg',
+  //   description: 'картина',
+  // },
+  {
     original: './images/portfolio/painting-3.jpg',
-    original2x: './images/portfolio/painting-3.jpg',
+    original2x: './images/portfolio/painting-3@2x.jpg',
     description: 'картина',
   },
-  { 
-    original: './images/portfolio/painting-4.jpg',
-    original2x: './images/portfolio/painting-4.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-5.jpg',
-    original2x: './images/portfolio/painting-5.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-6.jpg',
-    original2x: './images/portfolio/painting-6.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-7.jpg',
-    original2x: './images/portfolio/painting-7.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-8.jpg',
-    original2x: './images/portfolio/painting-8.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-9.jpg',
-    original2x: './images/portfolio/painting-9.jpg',
-    description: 'картина',
-  },
-  { 
-    original: './images/portfolio/painting-10.jpg',
-    original2x: './images/portfolio/painting-10.jpg',
-    description: 'картина',
-  }
-]
 
+  {
+    original: './images/portfolio/painting-h-11.jpg',
+    original2x: './images/portfolio/painting-h-11@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-12.jpg',
+    original2x: './images/portfolio/painting-h-12@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-13.jpg',
+    original2x: './images/portfolio/painting-h-13@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-14.jpg',
+    original2x: './images/portfolio/painting-h-14@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-15.jpg',
+    original2x: './images/portfolio/painting-h-15@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-16.jpg',
+    original2x: './images/portfolio/painting-h-16@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-17.jpg',
+    original2x: './images/portfolio/painting-h-17@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-19.jpg',
+    original2x: './images/portfolio/painting-h-19@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-20.jpg',
+    original2x: './images/portfolio/painting-h-20@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-21.jpg',
+    original2x: './images/portfolio/painting-h-21@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-22.jpg',
+    original2x: './images/portfolio/painting-h-22@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-23.jpg',
+    original2x: './images/portfolio/painting-h-23@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-h-24.jpg',
+    original2x: './images/portfolio/painting-h-24@2x.jpg',
+    description: 'картина',
+  },
+
+  {
+    original: './images/portfolio/painting-4.jpg',
+    original2x: './images/portfolio/painting-4@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-5.jpg',
+    original2x: './images/portfolio/painting-5@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-6.jpg',
+    original2x: './images/portfolio/painting-6@2x.jpg',
+    description: 'картина',
+  },
+  // {
+  //   original: './images/portfolio/painting-7.jpg',
+  //   original2x: './images/portfolio/painting-7.jpg',
+  //   description: 'картина',
+  // },
+  // {
+  //   original: './images/portfolio/painting-8.jpg',
+  //   original2x: './images/portfolio/painting-8.jpg',
+  //   description: 'картина',
+  // },
+  {
+    original: './images/portfolio/painting-9.jpg',
+    original2x: './images/portfolio/painting-9@2x.jpg',
+    description: 'картина',
+  },
+  // {
+  //   original: './images/portfolio/painting-10.jpg',
+  //   original2x: './images/portfolio/painting-10.jpg',
+  //   description: 'картина',
+  // },
+  {
+    original: './images/portfolio/painting-s-11.jpg',
+    original2x: './images/portfolio/painting-s-11@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-s-12.jpg',
+    original2x: './images/portfolio/painting-s-12@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-s-13.jpg',
+    original2x: './images/portfolio/painting-s-13@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-s-14.jpg',
+    original2x: './images/portfolio/painting-s-14@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-s-15.jpg',
+    original2x: './images/portfolio/painting-s-15@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-s-16.jpg',
+    original2x: './images/portfolio/painting-s-16@2x.jpg',
+    description: 'картина',
+  },
+
+  {
+    original: './images/portfolio/painting-v-12.jpg',
+    original2x: './images/portfolio/painting-v-12@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-13.jpg',
+    original2x: './images/portfolio/painting-v-13@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-14.jpg',
+    original2x: './images/portfolio/painting-v-14@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-15.jpg',
+    original2x: './images/portfolio/painting-v-15@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-16.jpg',
+    original2x: './images/portfolio/painting-v-16@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-17.jpg',
+    original2x: './images/portfolio/painting-v-17@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-18.jpg',
+    original2x: './images/portfolio/painting-v-18@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-19.jpg',
+    original2x: './images/portfolio/painting-v-19@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-20.jpg',
+    original2x: './images/portfolio/painting-v-20@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-21.jpg',
+    original2x: './images/portfolio/painting-v-21@2x.jpg',
+    description: 'картина',
+  },
+  {
+    original: './images/portfolio/painting-v-22.jpg',
+    original2x: './images/portfolio/painting-v-22@2x.jpg',
+    description: 'картина',
+  },
+];
 
 // -----------------------------------------------------------------------
 
 const paintingsGalleryRef = document.querySelector('.js-paintings-gallery');
 
-const paintingsGalleryMarkup = paintingsGalleryData.map( ({original, original2x, description}) => {
-  return `<li class="paintings__item" data-aos="fade-up">
+const paintingsGalleryMarkup = paintingsGalleryData
+  .map(({ original, original2x, description }) => {
+    return `<li class="paintings__item" data-aos="fade-up">
               <img
                 srcset="${original} 1x, ${original2x} 2x"
                 src="${original}"
                 alt="${description}"
                 loading="lazy"
               />
-            </li>`
-}).join('');
+            </li>`;
+  })
+  .join('');
 
 paintingsGalleryRef.innerHTML = paintingsGalleryMarkup;
-	
